da1bc17c6047da865e753952e3f96b5be808c65c
kumojustkumo-beep <kumojustkumo@gmail.com>
2026-06-03 11:41:41 -0400

    Delete config directory

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.DB_STORAGE,
-  logging: false
-});
-
-module.exports = sequelize;
